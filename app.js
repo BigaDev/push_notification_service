3000f17fe6344c69deb6ee4d8ff2e3b2ce86d3f4
Biga Gaber <ahmedgabr292@gmail.com>
2017-07-29 22:09:19 +0200

    setup bodyparser

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const app = express()
 const PORT = 8990
+const bodyParser = require('body-parser')
+
+// parse application/json
+app.use(bodyParser.json())
 
 app.listen(PORT, function () {
   console.log(`server listening on port ${PORT}!`)
