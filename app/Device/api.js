0909b21faa5fd13822383928bb359f215adf2fda
Biga Gaber <ahmedgabr292@gmail.com>
2017-07-30 18:43:40 +0200

    validate params - register api

--- app/Device/api.js
+++ app/Device/api.js
@@ -1,16 +1,35 @@
-// Device Controller
+// Device Model
+const DeviceModel = require('./model')
 
+// Device Controller
 function DeviceAPI(){
 
 }
 
-// Register API Handler
-// input: APN or GCM
-// process: validate token then save it if it didn't exist before
-// output: success => true if it saved or false with errors messages
-
+/* Register API Handler
+ input: APN or GCM
+ process: validate token then save it if it didn't exist before
+ output: success => true if it saved or false with errors messages
+ Note: I can get token OS type from request itself by check this field
+ req.headers['user-agent'] or use this npm package https://www.npmjs.com/package/platform
+ const platform = require('platform')
+ platform.os
+ but here for test I will send os type as parameter
+*/
 DeviceAPI.register = function(req, res){
-  res.json({'success': true})
+  // check exist of params
+  if(req.body.token === undefined)
+    return res.json({success: false, message: 'device token must be sent'})
+  if(req.body.os === undefined)
+    return res.json({success: false, message: 'device OS must be sent'})
+
+  //validate token
+  const token = req.body.token
+  const osType = req.body.os
+  if(DeviceModel.validateTokenType(token, osType) === null)
+    return res.json({success: false, message: 'device token not invalid'})
+
+  res.json({success: true})
 }
 
 module.exports = DeviceAPI
--- app/Device/model.js
+++ app/Device/model.js
@@ -2,4 +2,22 @@ function DeviceModel(){
 
 }
 
+function isIOS(token){
+  const iOSTokenPattern = /^[a-z0-9]{64}$/
+  let results = token.match(iOSTokenPattern)
+  return results != null && results.length > 0
+}
+
+function isGCM(token){
+  return token.length <= 256
+}
+
+DeviceModel.validateTokenType = function(token, type){
+  if(type === 'ios' && isIOS(token))
+    return 'ios'
+  if(type === 'android' && isGCM(token))
+    return 'android'
+  return null
+}
+
 module.exports = DeviceModel
